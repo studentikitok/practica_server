257c30185ca6f6fc72d679cde236a68ba5269701
StraponBace <ponomarev.tima2005@mail.ru>
2024-09-10 19:25:48 +0700

    post one"

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,14 +4,16 @@ namespace Controller;
 
 use Model\Post;
 use Src\View;
+use Src\Request;
 
 class Site
 {
-   public function index(): string
-   {
-       $posts = Post::all();
-       return (new View())->render('site.post', ['posts' => $posts]);
-   }
+    public function index(Request $request): string
+{
+   $posts = Post::where('id', $request->id)->get();
+   return (new View())->render('site.post', ['posts' => $posts]);
+}
+   
 
    public function hello(): string
    {
