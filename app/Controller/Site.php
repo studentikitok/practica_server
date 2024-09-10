aa25737a88aa20a3a08d58ea4b3932f578b39ba6
StraponBace <ponomarev.tima2005@mail.ru>
2024-09-10 19:07:15 +0700

    posts

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,14 +2,15 @@
 
 namespace Controller;
 
+use Model\Post;
 use Src\View;
 
 class Site
 {
    public function index(): string
    {
-       $view = new View();
-       return $view->render('site.hello', ['message' => 'index working']);
+       $posts = Post::all();
+       return (new View())->render('site.post', ['posts' => $posts]);
    }
 
    public function hello(): string
